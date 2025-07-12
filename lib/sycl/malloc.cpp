0c7eea44a79378c14cd9734308c8e51f7e88e2d6
admin <admin@localhost.localdomain>
2025-07-12 16:36:59 +0300

    malloc device seems to be working, but memory is not copied back to host

--- lib/sycl/malloc.cpp
+++ lib/sycl/malloc.cpp
@@ -1,25 +1,28 @@
 #include <charm/sycl.hpp>
 #include <iostream>
 #include "iris/iris_interface.hpp"
-CHARM_SYCL_BEGIN_NAMESPACE
 
+CHARM_SYCL_BEGIN_NAMESPACE
 namespace runtime {
 
 void *new_malloc(size_t count, size_t type_size){
 
     auto result = iris_interface_20000::init();
-
-    if (!result) {
-        std::cout << "IRIS init failed!";
+    std::cout << "After init" << std::endl;
+   
+    auto* mem = new iris_interface_20000::mem_t;
+    std::cout << "After memory object" << std::endl;
+    
+    auto create_result = iris_interface_20000::iris_mem_create(type_size * count, mem);
+    if (create_result != iris_interface_20000::SUCCESS) {
+        std::cout << "Memory creation failed!" << std::endl;
+        delete mem;
         return nullptr;
     }
     
-    iris_interface_20000::mem_t mem;
-    iris_interface_20000::iris_mem_create(type_size * count, &mem);
-    std::cout << "I am called";
-    return &mem;
+    std::cout << "Memory created successfully" << std::endl;
+    return mem;
 }
 
 }
-
 CHARM_SYCL_END_NAMESPACE
